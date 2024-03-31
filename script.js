4d364ef5766f3472bffa6db49928b77a5e70d028
anet-eny <anet.eny@gmail.com>
2024-03-31 14:08:29 +0200

    Create numberOfSquares logic

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-let numberOfSquares = 5
+let numberOfSquares = 100
+
+let squareContainer = document.querySelector(".container")
+
+let containerSize = 400
+let margin = 10
+let squaresPerRow = Math.floor(Math.sqrt(numberOfSquares))
+let squareSize = (containerSize - (squaresPerRow + 1) * margin) / squaresPerRow
+
+console.log(squaresPerRow)
+console.log(squareSize)
 
 for (let i = 0; i < numberOfSquares; i++ ){
     let square = document.createElement("div")
     square.classList.add("square-div")
 
-    let squareContainer = document.querySelector(".container")
+    square.style.width = squareSize + "px"
+    square.style.height = squareSize + "px"
+
+    
     squareContainer.appendChild(square)
 }
 
-
+let hover = document.querySelector("square-div")
+hover.addEventListener("mouseenter", (event) => {
+    event.target.style.color = "purple"
+})
