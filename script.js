2eb089bb90ac3d7eca2038f9df3e5d4e10ef73e1
anet-eny <anet.eny@gmail.com>
2024-04-04 15:59:42 +0200

    Create createGrid functionand update square style

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const squareContainer = document.querySelector(".container")
-squareContainer.style.width = "400px"
-squareContainer.style.height = "400px"
+const gridContainer = document.querySelector(".container")
+gridContainer.style.width = "400px"
+gridContainer.style.height = "400px"
+
+let squaresPerRow = 16 // default value
+
+function createGrid() {
+    for(let i = 0; i < squaresPerRow; i++) {
+        for(let j = 0; j < squaresPerRow; j++) {
+            let square = document.createElement("div")
+            square.classList.add("square-div")
+            square.style.width = (parseInt(gridContainer.style.width) / squaresPerRow) + "px"
+            square.style.height = (parseInt(gridContainer.style.width) / squaresPerRow) + "px"
+            gridContainer.appendChild(square)
+            
+        }
+    }
+}
+
+createGrid()
